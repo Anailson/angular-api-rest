f9748ef6daf9d91d7c43b1ca52b0069fdbe3b3c3
anailson ribeiro <arsantos315@gmail.com>
2022-07-12 15:14:06 -0300

    Criando a Barra de Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
+import { Router } from '@angular/router';
 import { LoginServiceService } from './Service/login-service.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Curso-Angular-Rest';
 
+  constructor (private router: Router){
+
+  }
+
+  ngOnInit(): void {
+
+    if(localStorage.getItem('token') == null){
+        this.router.navigate(['login']);  //CASO NÃO TENHA O TOKEN DE AUTENTICAÇÃO REDIRECIONAR PARA O LOGIN
+    }
+
+  }
+
+  public sair(){
+    localStorage.clear();
+    this.router.navigate(['login']);
+  }
 
 }
